f44d234e3b3977ef94ac0009957c3f9a0a17d52a
smohankarthi <30368934+smohankarthi@users.noreply.github.com>
2017-10-11 14:00:30 +0530

    Rename largestno1.c to leapyear1.c

---
+++ leapyear1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the Year");
+    scanf("%d",&n);
+    if(n%4==0)
+    {
+    printf("leap year");
+    }
+    else
+    {
+    printf("is not a leap year");
+    }
+
+}
